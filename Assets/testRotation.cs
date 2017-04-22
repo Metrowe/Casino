fd9736d7c925ab93b8f3b4943774b50ccbfa0b59
Shona Doran <shonamdoran@gmail.com>
2017-04-22 23:13:27 +0100

    merge messed everything up and gives advice that also fails so at least theres mulltiple versions of the messed up files

---
+++ Assets/testRotation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testRotation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        theta = Random.Range(3,8);
+        constant = 0.01f;
+	}
+
+    float theta;
+    float constant;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Rotate(0, theta, 0);
+
+        //theta -= constant;
+        
+        if (theta > 0)
+        {
+            theta -= constant;
+        }
+        else
+        {
+            theta = 0;
+        }
+        
+    }
+}
