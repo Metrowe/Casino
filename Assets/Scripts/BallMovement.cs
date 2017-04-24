4de50bc1e9e1d0513396562f446057496b736d06
Metrowe <emmetrowe@gmail.com>
2017-04-25 00:00:07 +0100

    Roullette wheel completely modeled

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -8,7 +8,7 @@ public class BallMovement : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        GetComponent<Rigidbody>().velocity = new Vector3(6f, 0, 0);
+        //GetComponent<Rigidbody>().velocity = new Vector3(6f, 0, 0);
     }
 
     // Update is called once per frame
